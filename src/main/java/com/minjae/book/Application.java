99c269b82a3d6c056710e23e4ed387b2f37ff1ac
toywar94 <toywar94@naver.com>
2021-09-28 23:22:05 +0900

    JPA CRUD

--- src/main/java/com/minjae/book/Application.java
+++ src/main/java/com/minjae/book/Application.java
@@ -3,8 +3,10 @@ package com.minjae.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
